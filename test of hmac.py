381eaaff14843dff583973fb8f835cef6677e2e2
asterwyx <cecilwyx@qq.com>
2019-06-29 23:03:41 +0800

    a test

--- test of hmac.py
+++ test of hmac.py
@@ -3,4 +3,3 @@ raw_message = b"Hello, world!"
 key = b"secret" # 需要注意的是传入的key和message都是bytes类型的
 encrypted_message = hmac.new(key, raw_message, digestmod='MD5')
 print(encrypted_message.hexdigest())
-# a small change

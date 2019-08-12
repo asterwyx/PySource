8d1e1c53a4f51431bbd999547491ab6a75123954
asterwyx <cecilwyx@gmail.com>
2019-08-12 10:05:02 +0800

    modify rules

--- exercise of doctest.py
+++ exercise of doctest.py
@@ -1,7 +1,7 @@
 def fact(n):
-    '''
+    """
     Calculate 1*2*...*n
-    
+
     >>> fact(1)
     1
     >>> fact(10)
@@ -10,12 +10,13 @@ def fact(n):
     Traceback (most recent call last):
     ...
     ValueError
-    '''
+    """
     if n < 1:
         raise ValueError()
     if n == 1:
         return 1
     return n * fact(n - 1)
+
 
 if __name__ == '__main__':
     import doctest
